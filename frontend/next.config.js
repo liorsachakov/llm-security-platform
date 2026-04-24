03de3b5aa30b6ed3b29a50ec56ca6021db0c40d7
liorsachakov <liorxm@gmail.com>
2026-04-24 12:50:53 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  env: {
+    BACKEND_API_BASE_URL: process.env.BACKEND_API_BASE_URL,
+    BACKEND_API_ORIGIN: process.env.BACKEND_API_ORIGIN,
+    BACKEND_API_STAGE: process.env.BACKEND_API_STAGE,
+  },
 }
 
 module.exports = nextConfig
