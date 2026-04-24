5437caa29f947c470508cfe6536df409e0809c11
liorsachakov <liorsac@wix.com>
2026-04-24 11:51:50 +0300

    adding standalone to nextConfig

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: false,
   },
 }
