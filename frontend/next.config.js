f2670a47875ff3c03ad2b933086c2a8d5f103a52
liorsachakov <liorxm@gmail.com>
2026-04-24 12:10:53 +0300

    removr standalone from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,5 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  outputFileTracingRoot: path.join(__dirname),
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: false,
